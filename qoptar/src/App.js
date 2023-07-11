bccf46a711fd084bf495376dd69b33d2539043ec
Aishwarea <73636667+Aishwarea@users.noreply.github.com>
2023-07-11 21:02:58 +0530

    Image Issue Rectified

--- qoptar/src/App.js
+++ qoptar/src/App.js
@@ -1,29 +1,37 @@
 import './App.css';
 import { Card } from "./components/Card"
+import Image1 from './images/image1.jpg';
+import Image2 from './images/image2.jpg';
+import Image3 from './images/image3.jpg';
 
 function App() {
   return (
+    <div>
+      <div>
+        <h1 font-style={'Poppins'}>Qoptar</h1>
+      </div>
     <div className='App'>
       <Card  
-      imgSrc='https://drive.google.com/file/d/1xN0RCxU-BcErW7PlMWiNtvC6K8w8sS_6/view?usp=sharing'
+      imgSrc={Image1}
       imgAlt='AerialSurveliance'
       title='Aerial Surveliance'
       description='Elevate your security game with our state-of-the-art aerial surveillance solutions.'
       />
       <Card  
-      imgSrc='https://drive.google.com/file/d/1pJKyoiImGDzPQx53YB6-2V7LaoJu8mbW/view?usp=drive_link'
+      imgSrc={Image2}
       imgAlt='BeginnerVideography'
       title='Beginner Videography'
       description='Capture breathtaking shots with ease, perfect 
       for beginner videographers.'
       />
       <Card  
-      imgSrc='https://drive.google.com/file/d/1pJKyoiImGDzPQx53YB6-2V7LaoJu8mbW/view?usp=sharing'
+      imgSrc={Image3}
       imgAlt='InspectionMadeEasy'
       title='Inspection Made Easy'
       description='Effortlessly inspect buildings, structures, and 
       landscapes with our powerful drones'
       />
+    </div>
     </div>
   );
 }

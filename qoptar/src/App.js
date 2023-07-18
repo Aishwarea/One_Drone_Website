5beda6fc14fa78f469fef3ac4cd4833e239dfe00
Aishwarea <73636667+Aishwarea@users.noreply.github.com>
2023-07-18 20:13:41 +0530

    Alignment

--- qoptar/src/App.js
+++ qoptar/src/App.js
@@ -1,37 +1,39 @@
-import './App.css';
-import { Card } from "./components/Card"
-import Image1 from './images/image1.jpg';
-import Image2 from './images/image2.jpg';
-import Image3 from './images/image3.jpg';
+import "./App.css";
+import { Card } from "./components/Card";
+import Image1 from "./images/image1.jpg";
+import Image2 from "./images/image2.jpg";
+import Image3 from "./images/image3.jpg";
 
 function App() {
   return (
     <div>
-      <div className='Title'>
-        <br/><h1>OneDrone Highlights</h1><br/>
+      <div className="Title">
+        <br />
+        <h1>OneDrone Highlights</h1>
+        <br />
       </div>
-    <div className='App'>
-      <Card  
-      imgSrc={Image1}
-      imgAlt='AerialSurveliance'
-      title='Aerial Surveliance'
-      description='Elevate your security game with our state-of-the-art aerial surveillance solutions.'
-      />
-      <Card  
-      imgSrc={Image2}
-      imgAlt='BeginnerVideography'
-      title='Beginner Videography'
-      description='Capture breathtaking shots with ease, perfect 
-      for beginner videographers.'
-      />
-      <Card  
-      imgSrc={Image3}
-      imgAlt='InspectionMadeEasy'
-      title='Inspection Made Easy'
-      description='Effortlessly inspect buildings, structures, and 
-      landscapes with our powerful drones'
-      />
-    </div>
+      <div className="App">
+        <Card
+          imgSrc={Image1}
+          imgAlt="AerialSurveliance"
+          title="Aerial Surveliance"
+          description="Elevate your security game with our state-of-the-art aerial surveillance solutions."
+        />
+        <Card
+          imgSrc={Image2}
+          imgAlt="BeginnerVideography"
+          title="Beginner Videography"
+          description="Capture breathtaking shots with ease, perfect 
+      for beginner videographers."
+        />
+        <Card
+          imgSrc={Image3}
+          imgAlt="InspectionMadeEasy"
+          title="Inspection Made Easy"
+          description="Effortlessly inspect buildings, structures, and 
+      landscapes with our powerful drones"
+        />
+      </div>
     </div>
   );
 }
--- qoptar/src/components/Card.jsx
+++ qoptar/src/components/Card.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./Card.css";
 
 export const Card = (props) => {
-    return (
+  return (
     <div className="Card">
-        {props.imgSrc && props.imgAlt && (
-        <img src={props.imgSrc} alt={props.imgAlt} className="CardImg"/>
-        )}
-        <br/>
-        {props.title && <h1 className="CardTitle">{props.title}</h1>}
-        <br/>
-        {props.description && <p className="CardDescription">{props.description}</p>}
-        <br/>
+      {props.imgSrc && props.imgAlt && (
+        <img src={props.imgSrc} alt={props.imgAlt} className="CardImg" />
+      )}
+      <br />
+      {props.title && <h1 className="CardTitle">{props.title}</h1>}
+      <br />
+      {props.description && (
+        <p className="CardDescription">{props.description}</p>
+      )}
+      <br />
     </div>
-    );
+  );
 };
